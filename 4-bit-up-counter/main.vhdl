d526db2ebd7431792b6529db9218769c64152404
j0hnbl4k3 <pandeyjatin47@gmail.com>
2021-03-17 19:17:11 +0530

    add more example

---
+++ 4-bit-up-counter/main.vhdl
@@ -0,0 +1,24 @@
+library IEEE; 
+use ieee.std_logic_1164.all; 
+use ieee.std_logic_unsigned.all;
+  
+entity counter is 
+   port(Clock, CLR : in std_logic; 
+      Q : out std_logic_vector(3 downto 0)
+   ); 
+end counter;  
+
+architecture behaviour of counter is 
+   signal tmp: std_logic_vector(3 downto 0); 
+begin 
+   process (Clock, CLR) 
+   
+   begin 
+      if (CLR = '1') then 
+         tmp < = "0000"; 
+      elsif (Clock event and Clock = '1') then 
+         mp <= tmp + 1; 
+      end if; 
+   end process; 
+   Q <= tmp; 
+end behaviour;
